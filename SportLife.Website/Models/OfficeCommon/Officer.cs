095bff60ec17e63935863ffac43a0d30d866c3a7
Trinity12 <bilyk.mariia@gmail.com>
2016-06-20 15:19:11 +0300

    And some view models

---
+++ SportLife.Website/Models/OfficeCommon/Officer.cs
@@ -0,0 +1,9 @@
+﻿using SportLife.Website.Resouses;
+
+namespace SportLife.Website.Models.OfficeCommon {
+    public class Officer {
+        public UserProfile Profile { get; set; }
+
+        public MainRoles Role { get; set; }
+    }
+}
---
+++ SportLife.Website/Models/OfficeCommon/UserProfile.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace SportLife.Website.Models.OfficeCommon {
+    public class UserProfile {
+        [ReadOnly(true)]
+        [DataType(DataType.Text)]
+        public string FullName { get; set; }
+
+        [DataType(DataType.Url)]
+        public UrlAttribute Avatar { get; set; }
+
+        public List<RoleViewModel> Roles { get; set; }
+    }
+}
