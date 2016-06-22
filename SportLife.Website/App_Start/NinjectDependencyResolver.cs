a969bb16d1321dfdbe499121566d8e44aeda83cf
Trinity12 <bilyk.mariia@gmail.com>
2016-06-22 10:27:52 +0300

    I a process of adding IoC

---
+++ SportLife.Website/App_Start/NinjectDependencyResolver.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using Ninject;
+
+namespace SportLife.Website {
+    public class NinjectDependencyResolver : IDependencyResolver {
+        private IKernel _kernel;
+
+        public NinjectDependencyResolver (IKernel kernelParam) {
+            _kernel = kernelParam;
+            AddBindings();
+        }
+
+        private void AddBindings () {
+            throw new NotImplementedException();
+        }
+
+        public object GetService ( Type serviceType ) {
+            return _kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices ( Type serviceType ) {
+            return _kernel.GetAll(serviceType);
+        }
+    }
+}
